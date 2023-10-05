a071bd705e5be862ee79ea7b1594f1e8540d61d1
Alok9809 <142798485+Alok9809@users.noreply.github.com>
2023-10-05 15:53:03 -0400

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+addition = a + b
+subtraction = a - b
+multiplication = a*b
+division = a/b
+print("the calculation of the number is",addition, subtraction , multiplication ,division )
